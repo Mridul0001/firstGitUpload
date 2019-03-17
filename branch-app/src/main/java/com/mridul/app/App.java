3bc395a40d093ebefa1463f8e568f6ec55dd3c36
Mridul0001 <mridulmahajan0001@gmail.com>
2019-03-17 18:26:26 +0530

    Commit in branch

--- branch-app/src/main/java/com/mridul/app/App.java
+++ branch-app/src/main/java/com/mridul/app/App.java
@@ -8,6 +8,6 @@ public class App
 {
     public static void main( String[] args )
     {
-        System.out.println( "Hello World!" );
+        System.out.println( "This file is commited under git branch" );
     }
 }
